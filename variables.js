fe4d5bf7e1c61e81fb9105e61164405346751742
Chakshita <desaichakshita@gmail.com>
2018-10-02 19:41:26 -0400

    starting our first program

---
+++ variables.js
@@ -0,0 +1,13 @@
+var myName = 'Chakshita Desai';
+
+myName = "Desai";
+
+console.log(myName);
+
+var declaredVar;
+
+console.log(declaredVar);
+
+declaredVar = 'Now I am declared!';
+
+console.log(declaredVar);
