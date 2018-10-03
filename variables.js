0a85ede1bf2af46976f4a58154a90175c2885170
Chakshita <desaichakshita@gmail.com>
2018-10-02 20:03:10 -0400

    more variables

--- variables.js
+++ variables.js
@@ -11,3 +11,50 @@ console.log(declaredVar);
 declaredVar = 'Now I am declared!';
 
 console.log(declaredVar);
+
+const myConstant = 'I cannot change'
+
+console.log(myConstant);
+
+//myConstant = "I can change!";
+
+/*
+I am a multiline comment
+
+*/
+
+let myLet = 'I can be changed!';
+
+console.log(myLet);
+
+myLet = 'I just changed!';
+
+console.log(myLet);
+
+//let define a string
+
+const myStr = 'I am a string';
+
+console.log(myStr);
+
+//delimiting the string
+
+const doubleQuoteStr = "I have double quotes";
+
+console.log(doubleQuoteStr);
+
+const myLife = "It's my life";
+
+console.log(myLife);
+
+const alsoMyLife = 'it's my life';
+console.log(alsoMyLife);
+const alsoMyLife = 'it\'s my life';
+console.log(alsoMyLife);
+//string interpoletion
+const templateStr = `" '' $(myLife}'')`;
+
+console.log(templateStr);
+
+const concatenatedStr = 'I am '+ 'concatenated';
+console.log(concatenatedStr);
